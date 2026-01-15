f16de57d308b81b27b63ad26aa3b2b56c2906583
bcsaalinas <betocnos2006@gmail.com>
2026-01-15 04:03:42 -0600

    Add Tailwind CSS integration and update layouts
    Integrated Tailwind CSS by adding configuration files (tailwind.config.js, postcss.config.js), input and output CSS files, and updating package dependencies. Refactored EJS layouts to use a new dashboard page and removed partials. Updated .gitignore for common Node.js and build artifacts.

--- src/app.js
+++ src/app.js
@@ -5,20 +5,23 @@ import indexRoutes from "./routes/index.routes.js";
 
 const app = express();
 
-// dirname replacement for ES modules
+// dirname setup
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// serve the static files
+import expressLayouts from "express-ejs-layouts";
+
+// static files
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(expressLayouts);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.set("layout", "layouts/main"); // default layout
 
-// temp test route to make sure everything works
-
+// routes
 app.use("/", indexRoutes);
 
 export default app;
--- src/controllers/dashboard.controllers.js
+++ src/controllers/dashboard.controllers.js
@@ -4,16 +4,13 @@ import {
 } from "../services/wakatime.service.js";
 
 export async function showHome(req, res) {
-  //getting all the time spent coding since the creation of the wakatime acc
   const stats = await getAllTimeSinceToday();
-
-  //get the summary from wakatime
   const summary = await getSummary();
   const dailyAvg = summary.daily_average.text_including_other_language;
 
   console.log(summary);
 
-  res.render("layouts/main", {
+  res.render("pages/dashboard", {
     timeText: stats.text,
     digital: stats.digital,
     totalSeconds: stats.total_seconds,
